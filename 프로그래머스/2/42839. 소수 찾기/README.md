4ea65d45a8acc2f7510e014adf0ee06bbba44d44
김지호 <68671529+gkdl7370@users.noreply.github.com>
2024-07-25 19:42:08 +0900

    [level 2] Title: 소수 찾기, Time: 1436.18 ms, Memory: 266 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/README.md
+++ 프로그래머스/2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839) 
 
 ### 성능 요약
 
-메모리: 246 MB, 시간: 1480.00 ms
+메모리: 266 MB, 시간: 1436.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 25일 18:46:44
+2024년 07월 25일 19:42:06
 
 ### 문제 설명
 
--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -7,6 +7,7 @@ class Solution {
     static Set<Integer> set = new HashSet<>();
     public int solution(String numbers) {
         int answer = 0;
+
         arr = new char[numbers.length()];
 
         for(int i=0; i<numbers.length(); i++){
@@ -16,40 +17,41 @@ class Solution {
         Arrays.sort(arr);
 
         for(int i=0; i<arr.length/2; i++){
-            char temp= arr[i];
+            char temp = arr[i];
             arr[i] = arr[arr.length-1-i];
             arr[arr.length-1-i] = temp;
         }
 
-        String str = new String(arr);
+        String str = new String(arr); //char배열에 있는 수 문자열로 만들어두기
 
 
-        int val = 0;
+        int val = 0; // 숫자로 최대값 만들기
         for(int i=0; i<arr.length; i++){
             int num = Character.getNumericValue(arr[i]);
             val = val*10+num;
         }
 
-        for(int i=0; i<=val; i++){
-            String temp = String.valueOf(i);
-            StringBuilder sb = new StringBuilder(temp);
+        for(int i=0; i<=val; i++){ //0~최대값까지
+            String temp = String.valueOf(i); //숫자를 문자열로 변환
+            StringBuilder sb = new StringBuilder(temp); //문자열 수정하기 위해서 StringBuilder 사용
             for(int k=0; k<str.length(); k++){
                 char ch = arr[k];
-                int index = sb.indexOf(String.valueOf(ch));
+                int index = sb.indexOf(String.valueOf(ch)); //sb에서 arr에 있는 수를 찾아본다
 
-                if(index != -1){
-                    sb.deleteCharAt(index);
+                if(index != -1){ //찾았으면
+                    sb.deleteCharAt(index); //지워버리기
                 }
 
-                if(sb.length() == 0){
-                    set.add(Integer.parseInt(temp));
-                    break;
+                if(sb.length() == 0){ //sb의 길이가 0이되면 다지워진거니까
+                    set.add(Integer.parseInt(temp)); //해당수는 조합상 만들어지는 수가 된다
+                    break; // 더이상 안찾아도됨
                 }
             }
         }
 
-        for(Integer num : set){
+        for(Integer num : set){ //set은 이런식으로 사용해야함
             int count = 0;
+
             if(num==2){
                 answer++;
                 continue;
@@ -60,7 +62,7 @@ class Solution {
                 }
             }
 
-            if(count == 0 && num > 1){
+            if(count == 0 && num > 1){ // 나눠지는게 없었다면 찾았다
                 answer++;
             }
         }
