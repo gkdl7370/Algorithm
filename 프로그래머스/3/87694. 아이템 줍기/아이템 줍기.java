177abedd8519c979db8f83cdefcbcc659201f590
김지호 <68671529+gkdl7370@users.noreply.github.com>
2024-08-27 17:28:21 +0900

    [level 3] Title: 아이템 줍기, Time: 2.45 ms, Memory: 70.4 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -1,46 +1,47 @@
 import java.util.LinkedList;
 import java.util.Queue;
-
 class Solution {
-     static int arr1[]={0,1,-1,0};
-    static int arr2[]={1,0,0,-1};
-    static boolean visited[][];
-    static int map[][];
+    int arr1[]={0,1,-1,0};
+    int arr2[]={1,0,0,-1};
+    int map[][];
+    boolean visited[][];
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
-     int answer = 0;
+        int answer = 0;
 
         map = new int[101][101];
         visited = new boolean[101][101];
+
         for(int i=0; i<rectangle.length; i++){
 
-            int x1 = rectangle[i][0]*2; //1
-            int y1 = rectangle[i][1]*2; //1
-            int x2 = rectangle[i][2]*2; //7
-            int y2 = rectangle[i][3]*2; //4
+            int x1 = rectangle[i][0]*2;
+            int y1 = rectangle[i][1]*2;
+            int x2 = rectangle[i][2]*2;
+            int y2 = rectangle[i][3]*2;
 
-            for(int j=x1; j<=x2; j++){
-                for(int k=y1; k<=y2; k++){
-                    if(map[j][k]==1) continue;
-                    map[j][k] = 1;
-                    if(j==x1 || j==x2 || k==y1 || k==y2) map[j][k] = 2;
+            for(int k=x1; k<=x2; k++){ //제일 바깥라인 2로 만들기 위함
+                for(int l=y1; l<=y2; l++){
+                    if(map[k][l] == 1) continue; // 이미 1로 칠해진곳은 내부에 그려진 라인이므로 패스
+                    map[k][l] = 1;
+
+                    if(k==x1 || k==x2 || l==y1 || l==y2){
+                        map[k][l] = 2;
+                    }
                 }
             }
-
         }
+
 
         answer = bfs(characterX*2,characterY*2,itemX*2,itemY*2);
         
-
         return answer;
     }
 
     public int bfs(int characterX, int characterY, int itemX, int itemY) {
-        Queue<Integer[]> q = new LinkedList<>();
-        q.add(new Integer[]{characterX,characterY,0});
-        visited[characterX][characterY] = true;
+        Queue<Integer[]> p = new LinkedList<>();
+        p.add(new Integer[]{characterX,characterY,0});
 
-        while (!q.isEmpty()){
-            Integer[] poll = q.poll();
+        while (!p.isEmpty()){
+            Integer[] poll = p.poll();
             int x = poll[0];
             int y = poll[1];
             int count = poll[2];
@@ -49,19 +50,22 @@ class Solution {
                 return count/2;
             }
 
-            for (int i=0; i<4; i++){
-                int nx = x+arr1[i];
-                int ny = y+arr2[i];
+            if(visited[x][y] == true) continue;
 
-                if(nx<0 || ny<0 || nx>map.length-1 || ny>map.length-1) continue;
+            visited[x][y] = true;
 
-                if(visited[nx][ny] == true || map[nx][ny] != 2) continue;
+            for(int i=0; i<4; i++){
+                int nx = x + arr1[i];
+                int ny = y + arr2[i];
 
-                visited[nx][ny] = true;
-                q.add(new Integer[]{nx,ny,count+1});
+                if(nx<0 || ny<0 || nx>=map.length || ny>=map.length) continue;
+
+                if(visited[nx][ny] == true) continue;
+
+                if(map[nx][ny] == 2) p.add(new Integer[]{nx,ny,count+1});
             }
         }
-
-        return 0;
+        
+        return -1;
     }
 }
