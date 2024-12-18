fc061af91b1e5574484d1c1a20e6be95d0335a84
김지호 <68671529+gkdl7370@users.noreply.github.com>
2024-12-18 15:54:19 +0900

    [Gold V] Title: 동전 2, Time: 116 ms, Memory: 14476 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -15,22 +15,20 @@ public class Main {
         int arr[] = new int[n+1];
         int dp[] = new int[k+1];
 
+        for(int i=1; i<=n; i++){
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
         Arrays.fill(dp,Integer.MAX_VALUE-1);
         dp[0] = 0;
 
         for(int i=1; i<=n; i++){
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-        
-        for(int i=1; i<=n; i++){
             for(int j=arr[i]; j<=k; j++){
-                dp[j] = Math.min(dp[j],dp[j-arr[i]]+1);
+                dp[j] = Math.min(dp[j],dp[j-arr[i]] + 1);
             }
         }
 
-        if(dp[k]==Integer.MAX_VALUE-1)
-            System.out.println(-1);
-        else
-            System.out.println(dp[k]);
+        if(dp[k] == Integer.MAX_VALUE-1) System.out.print(-1);
+        else System.out.print(dp[k]);
     }
 }
