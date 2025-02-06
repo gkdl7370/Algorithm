c8fb68d5ddaa806a70110e61d87ea0e2093bdccb
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-02-06 17:03:22 +0900

    [Gold III] Title: 허들 넘기, Time: 640 ms, Memory: 34148 KB -BaekjoonHub

---
+++ 백준/Gold/23286. 허들 넘기/허들 넘기.java
@@ -0,0 +1,51 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int n,m,t;
+    static int map[][];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        t = Integer.parseInt(st.nextToken());
+
+        map = new int[n+1][n+1];
+        int INF = Integer.MAX_VALUE;
+
+        for(int i=1; i<=m; i++){
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            int h = Integer.parseInt(st.nextToken());
+
+            map[x][y] = h;
+        }
+
+        for(int k=1; k<=n; k++) {
+            for (int i = 1; i <= n; i++) {
+                for (int j = 1; j <= n; j++) {
+                    if(map[i][k] != 0 && map[k][j] != 0) {
+                        if(map[i][j] == 0) map[i][j] = INF;
+                        map[i][j] = Math.min(map[i][j],Math.max(map[i][k], map[k][j]));
+                    }
+                }
+            }
+        }
+
+        for(int i=0; i<t; i++){
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            if(map[a][b] == 0) System.out.println(-1);
+            else System.out.println(map[a][b]);
+        }
+
+
+    }
+}
