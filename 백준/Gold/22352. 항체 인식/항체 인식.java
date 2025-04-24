fb3e189da9b958a2fbcf40b6f572cb1a07b411ef
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-04-24 16:26:56 +0900

    [Gold V] Title: 항체 인식, Time: 112 ms, Memory: 14520 KB -BaekjoonHub

---
+++ 백준/Gold/22352. 항체 인식/항체 인식.java
@@ -0,0 +1,93 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int arr1[]={0,1,-1,0};
+    static int arr2[]={1,0,0,-1};
+    static int n,m;
+    static int map[][];
+    static int map2[][];
+    static int exe[][];
+    static boolean vit[][];
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[n][m];
+        map2 = new int[n][m];
+        exe = new int[n][m];
+        vit = new boolean[n][m];
+
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        for(int i=0; i<n; i++){
+            st = new StringTokenizer(br.readLine());
+            for(int j=0; j<m; j++){
+                map2[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+
+        if(Arrays.deepEquals(map,map2)){
+            System.out.println("YES");
+            return;
+        }
+
+        boolean found = false;
+        boolean cntchk = false;
+        int count = 0;
+        for(int i=0; i<n; i++){
+            if(found == true) break;
+            if(cntchk == true) break;
+            for(int j=0; j<m; j++){
+                if(map[i][j] == map2[i][j]) continue;
+                if(vit[i][j] == true) continue;
+
+                if(count>0){
+                    cntchk = true;
+                    break;
+                }
+
+                int a = map2[i][j];
+                int b = map[i][j];
+                exe = map;
+                dfs(i,j,a,b);
+                count++;
+
+                if(Arrays.deepEquals(exe,map2)){
+                    System.out.println("YES");
+                    found = true;
+                    break;
+                }
+            }
+        }
+
+        if(!found) System.out.println("NO");
+    }
+
+    private static void dfs(int x, int y, int a, int b) {
+        vit[x][y] = true;
+        exe[x][y] = a;
+
+        for(int i=0; i<4; i++){
+            int nx = x + arr1[i];
+            int ny = y + arr2[i];
+
+            if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
+            if(vit[nx][ny] == true) continue;
+
+            if(exe[nx][ny] != b) continue;
+
+            dfs(nx, ny, a, b);
+        }
+    }
+}
