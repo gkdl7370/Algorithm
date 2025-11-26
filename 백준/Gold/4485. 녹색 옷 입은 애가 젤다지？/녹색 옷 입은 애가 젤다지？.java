ce5783be0f5c802821c8d260f362d0e6e71fbf18
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-11-26 11:24:24 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 208 ms, Memory: 19364 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.java
@@ -1,19 +1,17 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
 import java.util.PriorityQueue;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int dx[] = {1,0,-1,0};
-    static int dy[] = {0,1,0,-1};
+    static int dx[] = {1,-1,0,0};
+    static int dy[] = {0,0,1,-1};
+    static int n;
     static int map[][];
     static int dist[][];
-    static int t,n;
 
-    static class Point implements Comparable<Point>{
+    static class Point implements Comparable<Point> {
         int x;
         int y;
         int c;
@@ -29,60 +27,55 @@ public class Main {
             return this.c - o.c;
         }
     }
+   public static void main(String args[]) throws IOException {
+       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+       StringTokenizer st;
+       int t = 1;
+       while (true){
+           n = Integer.parseInt(br.readLine());
+           if(n == 0) break;
 
-    public static void main(String args[]) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        t = 1;
-        while (true){
+           map = new int[n][n]; //현재 지도
+           dist = new int[n][n]; //최소값을 저장할 지도
 
-            n = Integer.parseInt(br.readLine());
-            if (n==0) break;
+           for(int i=0; i<n; i++){
+               st = new StringTokenizer(br.readLine());
+               for(int j=0; j<n; j++){
+                   map[i][j] = Integer.parseInt(st.nextToken());
+                   dist[i][j] = Integer.MAX_VALUE; //최소값을 저장할거니까 최대값으로 초기 셋팅
+               }
+           }
 
-            map = new int[n][n];
-            dist = new int[n][n];
+           cost();
 
-            for(int i=0; i<n; i++){
-                st = new StringTokenizer(br.readLine());
-                for(int j=0; j<n; j++){
-                    map[i][j] = Integer.parseInt(st.nextToken());
-                    dist[i][j] = Integer.MAX_VALUE;
-                }
-            }
+           System.out.println("Problem "+ t + ": " + dist[n-1][n-1]);
+           t++;
+       }
+   }
 
-            dijkstra(0,0);
+    private static void cost() {
+        PriorityQueue<Point> q = new PriorityQueue<>(); //최소값을 갱신할거니까 우선순위 큐로 값저장
+        q.add(new Point(0,0,map[0][0])); //시작지점
+        dist[0][0] = map[0][0];
 
-            System.out.println("Problem " + t +": " + dist[n-1][n-1]);
-            t++;
-        }
-
-
-    }
-
-    private static void dijkstra(int a, int b) {
-        PriorityQueue<Point> p = new PriorityQueue<>();
-        p.add(new Point(a,b,map[a][b]));
-        dist[a][b] = map[a][b];
-
-        while (!p.isEmpty()){
-            Point poll = p.poll();
+        while (!q.isEmpty()){
+            Point poll = q.poll();
             int x = poll.x;
             int y = poll.y;
             int c = poll.c;
 
-            if(dist[x][y] < c) continue; //지금 가격보다 크면 필요없음
+            if(c > dist[x][y]) continue; //우선순위큐라 똑같은 좌표가 다시 들어올수있음 값 비교해서 필요없는지 확인해줘야함
 
             for(int i=0; i<4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if(nx<0 || ny<0 || nx>=n || ny>=n) continue; //범위 체크
+                if(nx<0 || ny<0 || nx>=n || ny>=n) continue; //범위체크
 
-                int nextCost = map[nx][ny] + c;
-
-                if(nextCost < dist[nx][ny]){
-                    dist[nx][ny] = nextCost;
-                    p.add(new Point(nx,ny,nextCost));
+                int nextCost = map[nx][ny] + c; //다음 위치를 도착했을때 값
+                if(dist[nx][ny]>nextCost){ // 지금 저장값보다 계산값이 작으면
+                    dist[nx][ny] = nextCost; // 값을 바꾼다 (최소값)
+                    q.add(new Point(nx,ny,dist[nx][ny]));
                 }
             }
         }
