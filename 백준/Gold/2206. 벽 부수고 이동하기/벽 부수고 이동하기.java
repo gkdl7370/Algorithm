ef4ebc9ab20801c64abe1591eedbf40e54bbb8d5
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-11-27 11:50:11 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 776 ms, Memory: 148288 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -13,32 +13,30 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         map = new int[n][m];
-        vit = new int[n][m][2];
+        vit = new int[n][m][2]; // 0벽 안부신 상태 1벽 부신 상태 확인용
 
-
-        for(int j=0; j<n; j++){
+        for(int i=0; i<n; i++){
             String str = br.readLine();
-            for(int k=0; k<m; k++){
-                map[j][k] = str.charAt(k) -'0';
-                vit[j][k][1] = -1;
-                vit[j][k][0] = -1;
+            for(int j=0; j<m; j++){
+                map[i][j] = str.charAt(j) - '0';
+                vit[i][j][0] = -1;
+                vit[i][j][1] = -1;
             }
         }
 
         int val = bfs();
-
-
         System.out.println(val);
     }
 
     private static int bfs() {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0,0});
-        vit[0][0][0] =  1;
+        q.add(new int[]{0,0,0}); //초기 상태
+        vit[0][0][0] = 1;
+        vit[0][0][1] = 1;
 
         while (!q.isEmpty()){
             int[] poll = q.poll();
@@ -49,31 +47,33 @@ public class Main {
             if(x == n-1 && y == m-1) return vit[x][y][broken];
 
             for(int i=0; i<4; i++){
-                int nx = x +dx[i];
-                int ny = y +dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
 
-                if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
-               
-                if(map[nx][ny] == 0){ //벽이 아닐때
-                    if(vit[nx][ny][broken] == -1){ //처음와보는거면
-                        vit[nx][ny][broken] = vit[x][y][broken] + 1;
-                        q.add(new int[]{nx,ny,broken});
-                    }
-                }
-                if(map[nx][ny] == 1){ //벽일때
-                    if(broken == 0){ //벽뿌 기회가 있다면
-                        if(vit[nx][ny][1] == -1){//처음 와봤다면 벽을 부수고
-                            vit[nx][ny][1] = vit[x][y][0] + 1;
+                if(nx<0||ny<0||nx>=n||ny>=m) continue; // 범위 체크
+
+                //벽인경우
+                if(map[nx][ny] == 1){
+                    if(broken == 0){ //벽 안부신상태여야 부실수있으니까
+                        if(vit[nx][ny][1] == -1){ //벽 부시고 도착한적이 없는지 확인
+                            vit[nx][ny][1] = vit[x][y][0] + 1; //이제 벽을 부시니까 전값은 무조건 안부신 상태의 값에 +1
                             q.add(new int[]{nx,ny,1});
                         }
+                    }
+                }
+
+                //벽이아닌경우
+                if(map[nx][ny] == 0){
+                    if(vit[nx][ny][broken] == -1){ //처음와봤으면 broken유무는 안중요함 벽을 안부실거니까
+                        vit[nx][ny][broken] = vit[x][y][broken] + 1;
+                        q.add(new int[]{nx,ny,broken});
                     }
                 }
             }
 
         }
+
         return -1;
     }
-
-
 }
 
