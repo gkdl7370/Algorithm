d7081aed686cd02ba26ba9e7870cc2f21930ba43
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-11-27 11:13:15 +0900

    [Silver I] Title: 단지번호붙이기, Time: 112 ms, Memory: 14196 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -6,71 +6,63 @@ import java.util.*;
 public class Main {
     static int dx[] = {1,0,-1,0};
     static int dy[] = {0,1,0,-1};
-    static int n;
-    static int count=0;
+    static int n,m;
     static int map[][];
     static boolean vit[][];
-    static List<Integer> list = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
 
-        n = Integer.parseInt(br.readLine());
         map = new int[n][n];
         vit = new boolean[n][n];
 
-        for(int i=0; i<n; i++){
+        List<Integer> list = new ArrayList<>();
+        for(int j=0; j<n; j++){
             String str = br.readLine();
-            for(int j=0; j<n; j++) {
-                map[i][j] = str.charAt(j) - '0';
+            for(int k=0; k<n; k++){
+                map[j][k] = str.charAt(k) -'0';
+            }
+        }
+        int count = 0;
+        for(int j=0; j<n; j++){
+            for(int k=0; k<n; k++){
+                if(map[j][k] == 1 && vit[j][k] != true){
+                    count++;
+                    int val = dfs(j,k); //탐색
+                    list.add(val);
+                }
             }
         }
 
-        search();
+        Collections.sort(list);
 
         System.out.println(count);
-        Collections.sort(list);
         for(int i=0; i<list.size(); i++){
             System.out.println(list.get(i));
         }
     }
 
-    private static void search() {
-        for(int i=0; i<n; i++){
-            for(int j=0; j<n; j++) {
-                if(map[i][j] == 1 && vit[i][j] == false){
-                    bfs(i,j);
-                    count ++;
-                }
-            }
-        }
-    }
+    private static int dfs(int a, int b) {
 
-    private static void bfs(int a, int b) {
-        Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{a,b});
-        vit[a][b] = true;
-        int num = 1;
+        vit[a][b] = true; //방문체크
+        int each = 1;
 
-        while (!q.isEmpty()){
-            int[] poll = q.poll();
-            int x = poll[0];
-            int y = poll[1];
+        for(int i=0; i<4; i++){
+            int x = a + dx[i];
+            int y = b + dy[i];
 
-            for(int i=0; i<4; i++){
-                int nx = x + dx[i];
-                int ny = y + dy[i];
+            if(x<0||y<0||x>=n||y>=n) continue;
+            if(vit[x][y] == true) continue;
+            if(map[x][y] == 0) continue;
 
-                if(nx<0 || ny<0 || nx>=n || ny>=n) continue;
-                if(vit[nx][ny] == true) continue;
-                if(map[nx][ny] == 0) continue;
+            each += dfs(x,y);
 
-                q.add(new int[]{nx,ny});
-                vit[nx][ny] = true;
-                num++;
-            }
         }
 
-        list.add(num);
+
+        return each;
     }
 
 
