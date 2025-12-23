16ef7ef91aefa6b2b86ba1598bd5e3f50876f9ad
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-12-23 18:17:41 +0900

    [Silver III] Title: N과 M (3), Time: 536 ms, Memory: 208208 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,37 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static int n, m;
+    static int map[];
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String args[]) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        map = new int[m];
+
+        dfs(0);
+
+        System.out.print(sb);
+    }
+    static void dfs(int dep){
+
+        if(dep == m){
+            for(int i=0; i<m; i++){
+               sb.append(map[i] + " ");
+            }
+            sb.append('\n');
+            return;
+        }
+
+        for(int i=1; i<=n; i++){
+            map[dep] = i;
+            dfs(dep+1);
+        }
+    }
+}
