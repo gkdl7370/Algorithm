1871da7492d0a47e796605ca1b704237a44d9a0f
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-12-31 11:57:26 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15828 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -2,24 +2,24 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static int n, m;
+    static int n,m;
+    static boolean vit[];
     static int map[];
-    static int vit[];
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
+        map = new int[m];
+        vit = new boolean[n+1];
 
-        map = new int[m+1];
-        vit = new int[n+1];
+        dfs(0, 1);
 
-        dfs(0,1);
     }
 
-    static void dfs(int depth, int now){
-        if(depth == m){
+    private static void dfs(int dep, int now){
+        if(dep == m){
             for(int i=0; i<m; i++){
                 System.out.print(map[i] + " ");
             }
@@ -28,11 +28,9 @@ public class Main {
         }
 
         for(int i=now; i<=n; i++){
-            if(vit[i] == 1) continue;
-            map[depth] = i;
-            vit[i] = 1;
-            dfs(depth+1,i);
-            vit[i] = 0;
+            map[dep] = i;
+            dfs(dep+1, i+1);
         }
+
     }
 }
