ec277c1f1aed55c1efe4dd8bd09e7df3f495a47b
김지호 <68671529+gkdl7370@users.noreply.github.com>
2025-12-31 12:03:15 +0900

    [Silver III] Title: N과 M (4), Time: 164 ms, Memory: 19852 KB -BaekjoonHub

--- 백준/Silver/15652. N과 M （4）/N과 M （4）.java
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -2,9 +2,9 @@
 import java.util.*;
 import java.io.*;
 public class Main {
-    static int n, m;
+    static int n,m;
+    static boolean vit[];
     static int map[];
-    static boolean vit[][];
     static StringBuilder sb = new StringBuilder();
     public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -12,28 +12,27 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-
         map = new int[m];
-        vit = new boolean[m][n+1];
+        vit = new boolean[n+1];
 
         dfs(0,1);
+
         System.out.print(sb);
     }
-    static void dfs(int dep, int now){
 
+    private static void dfs(int dep, int now){
         if(dep == m){
             for(int i=0; i<m; i++){
                 sb.append(map[i] + " ");
             }
-            sb.append('\n');
+            sb.append("\n");
             return;
         }
+
         for(int i=now; i<=n; i++){
-            if(vit[dep][i] == true) continue;
-            vit[dep][i] = true;
             map[dep] = i;
             dfs(dep+1, i);
-            vit[dep][i] = false;
         }
+
     }
 }
