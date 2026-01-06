4f853cce30f6fe8a08802665a9099c6c1d7af75c
김지호 <68671529+gkdl7370@users.noreply.github.com>
2026-01-06 18:35:33 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 732 ms, Memory: 148864 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.java
@@ -1,79 +1,88 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int dx[] = {1,0,-1,0};
-    static int dy[] = {0,1,0,-1};
-    static int n,m;
-    static int map[][];
-    static int vit[][][];
+    static int dx[] = {0,1,0,-1};
+    static int dy[] = {1,0,-1,0};
+    static int n, m;
+    static int[][] map;
+    static int[][][] dist;
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
+        //n*m행렬 최단경로 상하좌우 벽 한개까지 부시기 가능
+        //시간복잡도 1000 * 1000 1000000 < 100000000
+        //최단경로 bfs 사용
+        //벽 부셨는지 안부셨는지 체크  = 방문체크 3차원으로 작성 [x] [y] [벽뿌 확인]
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new int[n][m];
-        vit = new int[n][m][2]; // 0벽 안부신 상태 1벽 부신 상태 확인용
+        dist = new int[n][m][2];
 
         for(int i=0; i<n; i++){
             String str = br.readLine();
             for(int j=0; j<m; j++){
                 map[i][j] = str.charAt(j) - '0';
-                vit[i][j][0] = -1;
-                vit[i][j][1] = -1;
+                dist[i][j][0] = -1;
+                dist[i][j][1] = -1;
             }
         }
 
-        int val = bfs();
-        System.out.println(val);
+        int result = broken();
+
+
+        System.out.print(result);
     }
 
-    private static int bfs() {
+    private static int broken(){
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{0,0,0}); //초기 상태
-        vit[0][0][0] = 1;
-        vit[0][0][1] = 1;
+        q.add(new int[]{0,0,0}); //처음에는 벽을 부실필요 없으니 0으로 입력
+        dist[0][0][0] = 1; //시작점도 포함한다고 문제에 나옴
 
-        while (!q.isEmpty()){
+        while(!q.isEmpty()){
             int[] poll = q.poll();
             int x = poll[0];
             int y = poll[1];
-            int broken = poll[2];
+            int brk = poll[2]; //벽뿌 유무
+            int cost = dist[x][y][brk];
 
-            if(x == n-1 && y == m-1) return vit[x][y][broken];
+            if(x == n-1 && y == m-1) {
+                return cost;
+            }
 
             for(int i=0; i<4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
 
-                if(nx<0||ny<0||nx>=n||ny>=m) continue; // 범위 체크
+                if(nx<0 || ny<0 || nx>=n || ny>=m) continue;
 
-                //벽인경우
-                if(map[nx][ny] == 1){
-                    if(broken == 0){ //벽 안부신상태여야 부실수있으니까
-                        if(vit[nx][ny][1] == -1){ //벽 부시고 도착한적이 없는지 확인
-                            vit[nx][ny][1] = vit[x][y][0] + 1; //이제 벽을 부시니까 전값은 무조건 안부신 상태의 값에 +1
-                            q.add(new int[]{nx,ny,1});
-                        }
+                //방문체크
+                // 벽을 부시고 온 경우
+                if(brk == 1 && map[nx][ny] == 1) continue; //벽을 부셨으면 다음 벽이면 못감
+                else if(brk == 1 && map[nx][ny] == 0){ //벽을 부셨는데 다음 벽 아니면
+                    if(dist[nx][ny][1] == -1){ //벽 부셔서 와본적 없으면
+                        dist[nx][ny][1] = cost + 1;
+                        q.add(new int[]{nx,ny,1});
                     }
                 }
-
-                //벽이아닌경우
-                if(map[nx][ny] == 0){
-                    if(vit[nx][ny][broken] == -1){ //처음와봤으면 broken유무는 안중요함 벽을 안부실거니까
-                        vit[nx][ny][broken] = vit[x][y][broken] + 1;
-                        q.add(new int[]{nx,ny,broken});
+                // 벽을 안부시고 온 경우
+                if(brk == 0 && map[nx][ny] == 1){ //벽을 안부셨는데 벽이다
+                    if (dist[nx][ny][1] == -1) { //벽을 부시고 방문한적이 없으면
+                        dist[nx][ny][1] = cost + 1;
+                        q.add(new int[]{nx,ny,1});
+                    }
+                } else if(brk == 0 && map[nx][ny] == 0){ //벽 안부셨는데 벽이 아니다
+                    if (dist[nx][ny][0] == -1) { //벽을 부시고 방문한적이 없으면
+                        dist[nx][ny][0] = cost + 1;
+                        q.add(new int[]{nx,ny,0});
                     }
                 }
             }
-
         }
-
         return -1;
     }
 }
-
