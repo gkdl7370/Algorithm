66be21d76dc17ab014e04b85c42aa6488e16d110
김지호 <68671529+gkdl7370@users.noreply.github.com>
2026-02-10 21:40:19 +0900

    [Silver I] Title: 숨바꼭질, Time: 144 ms, Memory: 19724 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -1,48 +1,52 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int dx[] = {1,0,-1,0};
-    static int dy[] = {0,1,0,-1};
-    static int n,m;
-    static int map[];
-    static boolean vit[];
-    public static void main(String[] args) throws IOException {
+    static int n,k,time;
+    static int map[] = new int[100001];
+    public static void main(String args[]) throws IOException{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        map = new int[100001];
+        k = Integer.parseInt(st.nextToken());
+        time = 0;
 
         Queue<Integer> q = new LinkedList<>();
+
         q.add(n);
-        map[n] = 0;
+        while(!q.isEmpty()){
+            int x = q.poll();
 
-        while (!q.isEmpty()){
-            int now = q.poll();
-
-            if(now == m) break;
+            if(x == k) break;
 
             for(int i=0; i<3; i++){
-                int next = now;
                 if(i==0){
-                    next = now-1;
+                  int next = x - 1;
+                  if(next<0 || next>100000) continue;
+                  if(map[next] != 0) continue;
+                  map[next] = map[x] + 1;
+                  q.add(next);
                 } else if(i==1){
-                    next = now+1;
-                } else {
-                    next = now*2;
+                    int next = x + 1;
+                    if(next<0 || next>100000) continue;
+                    if(map[next] != 0) continue;
+                    map[next] = map[x] + 1;
+                    q.add(next);
+                } else if(i==2){
+                    int next = x * 2;
+                    if(next<0 || next>100000) continue;
+                    if(map[next] != 0) continue;
+                    map[next] = map[x] + 1;
+                    q.add(next);
                 }
-                if(next == n) continue;
-                if(next<0 || next>100000) continue;
-                if(map[next] != 0) continue;
-                map[next] = map[now] + 1;
-                q.add(next);
             }
+
+
         }
 
-        System.out.print(map[m]);
+        System.out.print(map[k]);
+
     }
 }
-
