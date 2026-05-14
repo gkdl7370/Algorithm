43ae53c5da70ded0b5ad52d1b87fa313f1d8a7e1
김지호 <68671529+gkdl7370@users.noreply.github.com>
2026-05-14 12:24:58 +0900

    [level 1] Title: 신규 아이디 추천, Time: 9.82 ms, Memory: 62.2 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,21 +1,25 @@
 class Solution {
     public String solution(String new_id) {
         
-        String answer = new_id.toLowerCase();
-        answer = answer.replaceAll("[^a-z0-9-_.]","");
-        answer = answer.replaceAll("[.]{2,}",".");
-        answer = answer.replaceAll("^[.]|[.]$","");
-        if(answer.equals("")){
-            answer = "a";
+        String str = new_id.toLowerCase();
+        
+        str = str.replaceAll("[^a-z0-9-_.]","");
+        str = str.replaceAll("[.]{2,}",".");
+        str = str.replaceAll("^[.]|[.]$","");
+        
+        if(str.isEmpty()){
+            str = "a";
         }
-        if(answer.length() >= 16){
-            answer = answer.substring(0,15);
-            answer = answer.replaceAll("[.]$","");
+        if(str.length() >= 16){
+            str = str.substring(0,15);
+            str = str.replaceAll("[.]$","");
         }
-        while(answer.length() < 3){
-            answer += answer.charAt(answer.length() - 1);
+        while(str.length() < 3){
+            str += str.charAt(str.length() - 1);
         }
         
-        return answer;
+        return str;
+                            
+        
     }
 }
